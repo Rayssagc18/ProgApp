888fb840efa58f5f5719bdfdc4a8764d66584305
GuilhermSA07 <Guilherm.Araujo.Silva@gmail.com>
2019-07-03 17:07:27 -0300

    Html/Css

---
+++ Se.js
@@ -0,0 +1,9 @@
+var valor = tchau;
+var oi = "Bom dia caralho";
+var tchau = "Boa noite porra";
+
+if (valor == oi){
+    console.log(oi);
+} else {
+    console.log(tchau);
+}
---
+++ Teste.js
@@ -0,0 +1,12 @@
+var langs = ["html", "css", "Javascript"]
+var localizacao = langs[0]
+    langs [langs.length] = "php"
+    //imprimir a função
+
+    var langs = ["html", "css", "Javascript"];
+    var last = langs.pop();
+    //remove o último elemento do vetor e o retorna
+    console.log(localizacao)
+
+    var langs = ["html", "css", "Javascript"];
+    var notoTam = langs.unshift("php");
