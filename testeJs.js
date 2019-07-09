d3269a9e203e04c1324f982a7dfe64722a5bb052
Rayssagc18 <51210408+Rayssagc18@users.noreply.github.com>
2019-07-09 15:50:27 -0300

    Add files via upload

---
+++ testeJs.js
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>Javascript fora do Html</title>
+        <meta charset="utf-8"></meta>
+    </head>
+    <body>
+        <div>
+           <div>
+              <label for="string">Coloque um texto</label>
+              <input type="text" id="string"/>
+              </div>
+              <div>
+                <label for="substring">Parte que desejo encontrar</label>
+                <input type="text" id="substring"/>
+              </div>
+              <div>
+                  <p>Seja diferente!</p>
+                  <button id="botaoEncontrar" onclick="localizarString()"
+                  Ache</button>
+                  <span id="resultado"></span>
+                  </div>
+              </div>
+              </body>
+              <script type="text/javascript" src=Js/testeJs.js></script>
+</html>
